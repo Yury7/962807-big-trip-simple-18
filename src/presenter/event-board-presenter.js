76381244870aacdd10a63ed24ff544b9e01e38b7
Yury <marchenko.yr@yandex.ru>
2022-09-18 16:04:53 +0300

    Реализация смены отображения точки маршрута на форму редактирования

--- src/presenter/event-board-presenter.js
+++ src/presenter/event-board-presenter.js
@@ -4,6 +4,7 @@ import EventListView from '../view/event-list-view.js';
 import EventEditView from '../view/event-edit-view.js';
 import EventItemView from '../view/event-item-view.js';
 import {render} from '../render.js';
+import {isEscapeKey} from '../utils.js';
 
 
 export default class EventBoardPresenter {
@@ -12,6 +13,62 @@ export default class EventBoardPresenter {
   #events = null;
   #eventListComponent = new EventListView();
   #filterContainer = document.querySelector('.trip-controls__filters');
+  //-------------------------------------------
+  #renderEvent = (event) => {
+
+    const eventItemComponent = new EventItemView(
+      event
+      // getEventOffers(event.offers, this.#offers),
+      // getEventDestination(event.destination, this.#destinations)
+    );
+
+    const eventEditComponent = new EventEditView(
+      event
+      // getEventOffers(event.offers, this.#offers),
+      // getEventDestination(event.destination, this.#destinations),
+      // this.#offers
+    );
+
+    render(eventItemComponent, this.#eventListComponent.element);
+
+    const replaceFormToItem = () => {
+      this.#eventListComponent.element.replaceChild(
+        eventItemComponent.element,
+        eventEditComponent.element
+      );
+    };
+
+    const onEscKeyDown = (evt) => {
+      if (!isEscapeKey(evt)) {return;}
+      replaceFormToItem();
+      document.removeEventListener('keydown', onEscKeyDown);
+    };
+
+    const replaceEventToForm = () => {
+      this.#eventListComponent.element.replaceChild(
+        eventEditComponent.element,
+        eventItemComponent.element
+      );
+
+      document.addEventListener('keydown', onEscKeyDown);
+    };
+
+    eventItemComponent.element
+      .querySelector('.event__rollup-btn')
+      .addEventListener('click', replaceEventToForm);
+
+
+    eventEditComponent.element.addEventListener('submit', (evt) => {
+      evt.preventDefault();
+      replaceFormToItem();
+    });
+
+
+    eventEditComponent.element
+      .querySelector('.event__rollup-btn')
+      .addEventListener('click', replaceFormToItem);
+  };
+
   init = (eventsContainer, eventsModel) => {
     this.#eventsContainer = eventsContainer;
     this.#eventsModel = eventsModel;
@@ -19,9 +76,11 @@ export default class EventBoardPresenter {
     render(new FilterView(), this.#filterContainer);
     render(new SortView(), this.#eventsContainer);
     render(this.#eventListComponent, this.#eventsContainer);
-    render(new EventEditView(this.#events[0]), this.#eventListComponent.element);
+
+    // console.log(eventItemComponent);
+
     for (let i = 1; i < this.#events.length - 1; i++){
-      render(new EventItemView(this.#events[i]), this.#eventListComponent.element);
+      this.#renderEvent(this.#events[i]);
     }
   };
 }
